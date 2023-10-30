deaf28aa5f875ec6b34d660332df9000a59d7cc7
testGit <test@Git.com>
2023-10-30 19:19:21 +0530

    readme and registration form

--- snow-tails/README.md
+++ snow-tails/README.md
@@ -6,3 +6,50 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+#RESOURCES:
+
+    button event
+    <
+        https://legacy.reactjs.org/docs/handling-events.html
+    >
+
+#Configuration [package.json]
+vite + react
+tailwind css
+Nodemon
+
+#Core Features
+
+    Functional
+
+    <
+        Nav bar
+        video section
+        Iformation + registration
+        About + registration
+        Cause
+        Testimonials
+        Travels and stay
+        Footer Registration section
+        partners
+        Contact us
+    >
+
+    <
+        Registration form function
+    >
+
+
+
+    User Flow:
+
+    <
+        User visits the website
+        browse through different sections
+        clicks Register
+        Takes to a google form
+        Fills details
+        Registration complete
+        Email and whatsapp notification
+    >
--- snow-tails/src/App.jsx
+++ snow-tails/src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
 import "./App.css";
+import Form from "./form";
 
 function App() {
   return (
@@ -7,13 +7,25 @@ function App() {
     <>
       <div class="w-full shadow-[rgba(0,_0,_0,_0.25)_0px_25px_50px_-12px]">
         <div class="ml-4 h-14 max-w-6xl">
-          <ul class="relative mx-12 flex h-full flex-row items-center justify-between text-[16px] font-medium sm:text-[18px]">
+          <ul class="relative mx-12 flex h-full flex-row items-center justify-between text-[16px] font-medium sm:text-[12px]">
             <li class="hover:cursor-pointer">Logo</li>
             <li class="hover:cursor-pointer">Home</li>
             <li class="hover:cursor-pointer">Services</li>
             <li class="hover:cursor-pointer">Contact</li>
           </ul>
         </div>
+      </div>
+
+      <div
+        id="register"
+        class="container none p-10 max-h-screen max-w-screen flex align-items-center justify-center bg-slate-100"
+      >
+        <button
+          class="middle none center w-full rounded-lg bg-pink-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+          data-ripple-light="true"
+        >
+          Register
+        </button>
       </div>
 
       <div
@@ -34,12 +46,8 @@ function App() {
       >
         Testimonials
       </div>
-
-      <div
-        id="video_section"
-        class="container none max-h-screen max-w-screen w-screen h-screen flex align-items-center justify-center bg-slate-100"
-      >
-        Footer & Registration form
+      <div>
+        <Form />
       </div>
     </>
 
